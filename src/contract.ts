54333300a8fe98d128a07283798cd38004e8a9e0
Santiago <salviega6@gmail.com>
2022-11-16 07:49:08 -0500

    [ADD] First commit

--- src/contract.ts
+++ src/contract.ts
@@ -1,77 +1,42 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  Contract,
-  Approval,
-  ApprovalForAll,
-  Bought,
-  Offerd,
-  Transfer
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { Bought, Offerd } from "../generated/Contract/Contract"
+import { DataBought, DataOfferd } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  export function handleBought(event: Bought): void {
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    let id = event.params._itemId.toString()
+    let entity = DataBought.load(id)
+  
+    if (!entity) entity = new DataBought(id)
+  
+    entity.itemId = event.params._itemId
+    entity.nft = event.params._nft.toHexString()
+    entity.tokenId = event.params._tokenId
+    entity.tokenURI = event.params._tokenURI
+    entity.price = event.params._price
+    entity.artist = event.params._artist.toHexString()
+    entity.taxFee = event.params._taxFee
+    entity.addressTaxFeeToken = event.params._addressTaxFeeToken.toHexString()
+    entity.buyer = event.params._buyer.toHexString()
+  
+    entity.save()
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.EPNS_COMM_ADDRESS(...)
-  // - contract.ItemCounter(...)
-  // - contract.account(...)
-  // - contract.amount(...)
-  // - contract.balanceOf(...)
-  // - contract.erc777(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.items(...)
-  // - contract.mint(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenApproved(...)
-  // - contract.tokenIdCounter(...)
-  // - contract.tokenURI(...)
-  // - contract.tokens(...)
-}
-
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
-export function handleBought(event: Bought): void {}
-
-export function handleOfferd(event: Offerd): void {}
-
-export function handleTransfer(event: Transfer): void {}
+  
+  export function handleOfferd(event: Offerd): void {
+  
+    let id = event.params._itemId.toString()
+    let entity = DataOfferd.load(id)
+  
+    if (!entity) entity = new DataOfferd(id)
+  
+    entity.itemId = event.params._itemId
+    entity.nft = event.params._nft.toHexString()
+    entity.tokenId = event.params._tokenId
+    entity.tokenURI = event.params._tokenURI
+    entity.price = event.params._price
+    entity.artist = event.params._artist.toHexString()
+    entity.taxFee = event.params._taxFee
+    entity.addressTaxFeeToken = event.params._addressTaxFeeToken.toHexString()
+    entity.seller = event.params._seller.toHexString()
+  
+    entity.save()
+  }
